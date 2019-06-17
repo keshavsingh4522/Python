05ea7600771fbdbfd6f297d0e4dd5cf4af5cf5de
Keshav Singh <43788985+keshavsingh4522@users.noreply.github.com>
2019-06-17 10:58:36 +0530

    Create 2_1.py

---
+++ pattern/2_1.py
@@ -0,0 +1,12 @@
+n=int(input("enter a number: "))
+for i in range(1,n+1):
+ print(" "*(n-i),"*"*(2*i-1),sep="")
+ '''
+ output:
+ enter a number: 5
+    *
+   ***
+  *****
+ *******
+*********
+ '''
