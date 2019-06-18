ebc53c461b767faa0998fd4a0903ba3826c9ca60
Keshav Singh <43788985+keshavsingh4522@users.noreply.github.com>
2019-06-18 13:11:13 +0530

    Create prime_number_check.py

---
+++ prime_number_check.py
@@ -0,0 +1,14 @@
+n=int(input("enter a number to check prime or not:  "))
+c=0
+for i in range(2,n//2+1):
+ if(n%i==0):
+  c+=1
+  print("not prime")
+  break
+else:
+ print("prime")
+'''
+output:
+enter a number to check prime or not:  7
+prime
+'''
