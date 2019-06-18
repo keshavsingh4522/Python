ccd2c216e17111677b7d395842486a18a11136db
Keshav Singh <43788985+keshavsingh4522@users.noreply.github.com>
2019-06-18 12:06:02 +0530

    Create 1_1_3n_4.py

---
+++ pattern/1_1_3n_4.py
@@ -0,0 +1,23 @@
+n=int(input("enter a number: "))
+for i in range(n,0,-1):
+  for j in range(n,0,-1):
+   if i>j:
+    print(i,' ',sep='',end="")
+   else:
+    print(j,' ',sep='',end="")
+  for j in range(2,n+1):
+    if i>j:
+      print(i,' ',end='',sep="")
+    else:
+      print(j,' ',end='',sep="")
+  print()
+ '''
+ output:
+ enter a number: 6
+6 6 6 6 6 6 6 6 6 6 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 2 1 2 3 4 5 6
+ '''
