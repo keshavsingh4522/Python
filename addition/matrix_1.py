045eae6738c03ed32590f344ebd6942b93772407
Keshav Singh <keshavsingh4522@gmail.com>
2019-06-22 11:08:48 +0530

    Update matrix_1.py

--- addition/matrix_1.py
+++ addition/matrix_1.py
@@ -19,22 +19,24 @@ for i in range(r):
 #addition of two matrix
 print('<<<<< addition of two matrix >>>>>')
 for i in range(r):
+   l=[]
    for j in range(c):
-    a.append(int(m1[i][j])+int(m2[i][j]))
+    l.append(int(m1[i][j])+int(m2[i][j]))
+   a.append(l)
 print(a)
 '''
 output:
 enter number of rows and columns of matrix: 2 2
 <<<<< enter element of matrix1 >>>>>
-enter in m1[0][0]: 22
-enter in m1[0][1]: 11
-enter in m1[1][0]: 22
-enter in m1[1][1]: 33
+enter in m1[0][0]: 21
+enter in m1[0][1]: 22
+enter in m1[1][0]: 23
+enter in m1[1][1]: 24
 <<<<< enter element of matrix2 >>>>>
-enter in m2[0][0]: 21
-enter in m2[0][1]: 33
-enter in m2[1][0]: 45
-enter in m2[1][1]: 33
+enter in m2[0][0]: 1
+enter in m2[0][1]: 2
+enter in m2[1][0]: 3
+enter in m2[1][1]: 4
 <<<<< addition of two matrix >>>>>
-[43, 44, 67, 66]
+[[22, 24], [26, 28]]
 '''
