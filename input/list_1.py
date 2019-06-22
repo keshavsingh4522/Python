33dd4b730ad7c147b177f7368124849e07becaab
Keshav Singh <keshavsingh4522@gmail.com>
2019-06-22 10:33:55 +0530

    Create list_1.py

---
+++ input/list_1.py
@@ -0,0 +1,16 @@
+n=int(input("enter number of elements: "))
+l=[]
+for i in range(n):
+  l.append(int(input(f"enter l[{i}]: ")))
+print(l)
+'''
+output:
+enter number of elements: 6
+enter l[0]: 23
+enter l[1]: 11
+enter l[2]: 67
+enter l[3]: 889
+enter l[4]: 342
+enter l[5]: 23
+[23, 11, 67, 889, 342, 23]
+'''
