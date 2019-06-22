42d238d5789bfb4715b4592a83bf247fe9a8d9a0
Keshav Singh <keshavsingh4522@gmail.com>
2019-06-22 11:29:03 +0530

    Create matrix_1_simple.py

---
+++ addition/matrix_1_simple.py
@@ -0,0 +1,31 @@
+r,c=map(int,input('enter number of rows and columns of matrix: ').split())
+m1=[]
+m2=[]
+add=[]
+print('<<<<< enter element of matrix1 >>>>>')
+for i in range(r):
+ l1=[]
+ l2=[]
+ la=[]
+ for j in range(c):
+  a,b=map(int,input(f"enter in m1[{i}][{j}] m2[{i}][j]: ").split())
+  l1.append(a)
+  l2.append(b)
+  la.append(a+b)
+ m1.append(l1)
+ m2.append(l2)
+ add.append(la)
+
+print('matrix1 : ',m1,'\nmatrix2 : ',m2,'\naddition: ',add,sep='')
+'''
+output:
+enter number of rows and columns of matrix: 2 2
+<<<<< enter element of matrix1 >>>>>
+enter in m1[0][0] m2[0][j]: 1 2
+enter in m1[0][1] m2[0][j]: 3 4
+enter in m1[1][0] m2[1][j]: 5 6
+enter in m1[1][1] m2[1][j]: 7 8
+matrix1 : [[1, 3], [5, 7]]
+matrix2 : [[2, 4], [6, 8]]
+addition: [[3, 7], [11, 15]]
+'''
