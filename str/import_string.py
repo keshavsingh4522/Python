26a827c4474620b5e0d7525f04127b768c6979a1
Keshav Singh <keshavsingh4522@gmail.com>
2019-06-29 01:00:32 +0530

    Create import_string.py

---
+++ str/import_string.py
@@ -0,0 +1,17 @@
+>>> import string
+>>> a=string.ascii_lowercase
+>>> a
+'abcdefghijklmnopqrstuvwxyz'
+>>> a=string.ascii_uppercase
+>>> a
+'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+>>> a=string.ascii_letters
+>>> a
+'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+>>> a=string.digit
+>>> a=string.digits
+>>> a
+'0123456789'
+>>> a=string.hexdigits
+>>> a
+'0123456789abcdefABCDEF'
