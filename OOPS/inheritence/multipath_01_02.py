00ae31a50d7d3cff5dc76de69495c701c58dd32a
Keshav Singh <keshavsingh4522@gmail.com>
2019-07-19 14:22:35 +0530

    Create multipath_01_02.py

---
+++ OOPS/inheritence/multipath_01_02.py
@@ -0,0 +1,35 @@
+class E:
+ pass
+class A:
+ def __init__(self):
+  print('class A')
+class B(A):
+ def __init__(self):
+  super().__init__()
+  print('class B')
+class C(A):
+ def __init__(self):
+  super().__init__()
+  print('class C')
+class D(C,B):
+ def __init__(self):
+  super().__init__()
+  print('class D')
+class E(C):
+ def __init__(self):
+  super().__init__()
+  print('class E')
+class F(D,E):
+ def __init__(self):
+  super().__init__()
+  print('class F')
+f=F()
+'''
+output:
+class A
+class B
+class C
+class E
+class D
+class F
+'''
