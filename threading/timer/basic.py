c80210bd052c94ae3e24fe90ce257e658b8e044b
Keshav Singh <keshavsingh4522@gmail.com>
2019-07-31 14:04:36 +0530

    Update basic.py

--- threading/timer/basic.py
+++ threading/timer/basic.py
@@ -6,14 +6,22 @@ def f():
     t.sleep(1)
     lg.debug('hello')
 def g():
-    t.sleep(3)
+    t.sleep(1)
     lg.debug('keshav')
 t1=th.Timer(1,f)
 t2=th.Timer(1,g)
+# t2=th.Timer(2,g)
 t1.start()
 t2.start()
+t1.join()
 t2.cancel()
 '''
 output:
-Thread-19 hello
+Thread-55 hello   
+Thread-56 keshav
+'''
+#
+'''
+output:
+Thread-55 hello   
 '''
