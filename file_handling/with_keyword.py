a9fe8eb88fdc6a9b39b08f403773032c925f830b
Keshav Singh <keshavsingh4522@gmail.com>
2019-07-31 18:49:00 +0530

    Create with_keyword.py

---
+++ file_handling/with_keyword.py
@@ -0,0 +1,9 @@
+with open('keshav.txt','w') as file:
+    file.write(input('write in file here: '))
+with open('keshav.txt','r') as file:
+    print(file.read())
+'''
+output:
+write in file here: kya karoo hi mera dil ro gya
+kya karoo hi mera dil ro gya
+'''
