90510810e3c231ee2517e3ad21e260d39afe6b9e
Keshav Singh <keshavsingh4522@gmail.com>
2019-09-22 19:03:38 +0530

    argument passing by sys module through cmd

---
+++ cmd/cmd_sys.py
@@ -0,0 +1,19 @@
+# cmd by sys module
+import sys
+print('file name: ',sys.argv[0])
+print('1st argument: ',sys.argv[1])
+print('2nd argument: ',sys.argv[2])
+print('3rd argument: ',sys.argv[3])
+print('sys.arg: ',sys.argv)
+'''
+command:
+python3 cmd_sys.py keshav singh jadon
+
+output:
+file name:  cmd_sys.py
+1st argument:  keshav
+2nd argument:  singh
+3rd argument:  jadon
+sys.arg:  ['cmd_sys.py', 'keshav', 'singh', 'jadon']
+
+'''
