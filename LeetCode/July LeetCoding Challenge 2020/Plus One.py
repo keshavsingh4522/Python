7fef9a3dabe344d4194d1e90a89713dafc53d2f1
Keshav Singh <keshavsingh4522@gmail.com>
2020-07-12 15:07:21 +0530

    LeetCode July 30 day

---
+++ LeetCode/July LeetCoding Challenge 2020/Plus One.py
@@ -0,0 +1,23 @@
+'''
+Given a non-empty array of digits representing a non-negative integer, increment one to the integer.
+
+The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+
+You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+Input: [1,2,3]
+Output: [1,2,4]
+Explanation: The array represents the integer 123.
+Example 2:
+
+Input: [4,3,2,1]
+Output: [4,3,2,2]
+Explanation: The array represents the integer 4321.
+'''
+a=list(map(int,input().split()))
+l=list(str(int(''.join((map(str,a))))+1))
+for i in range(len(l)):
+    l[i]=int(l[i])
+print(l)
