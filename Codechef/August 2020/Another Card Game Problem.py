32634a5b31284612053cd8337cd54c6b6046bd1d
Keshav Singh <keshavsingh4522@gmail.com>
2020-08-14 18:28:25 +0530

    Live Contest solution deleted

--- Codechef/August 2020/Another Card Game Problem.py
+++
@@ -1,9 +0,0 @@
-import math
-for _ in range(int(input())):
-    c,r=map(int,input().split())
-    movc=math.ceil(c/9)
-    movr=math.ceil(r/9)
-    if movr<=movc:
-        print(1,movr)
-    else:
-        print(0,movc)
