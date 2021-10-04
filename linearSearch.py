85f18e86d9fc7bb576eca42eb9b84e42924b0e5f
Jatin Jain <jatinalwar2001@gmail.com>
2021-10-04 12:28:58 +0530

    Update linearSearch.py

--- linearSearch.py
+++ linearSearch.py
@@ -1,8 +1,11 @@
 def Linearsearch(arr, x):
 
-	for i in range(len(arr)):
+	for i,item in enumerate(arr):
 
 		if arr[i] == x:
 			return i
 
 	return -1
+
+arr = [4,0,9,5,1,3]
+print(Linearsearch(arr, 5))
