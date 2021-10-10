020f58998d9b24557c78a4217a7142c3d311f3b1
ftorto <dev.torto@gmail.com>
2021-10-10 21:08:29 +0200

    add find next palindrome number

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,16 @@
-def checkIfPalindrome(s):
+def check_if_palindrome(s):
     return s == s[::-1]
     
 s = "A Santa Lived As a Devil At NASA"
-ans = checkIfPalindrome(s)
+ans = check_if_palindrome(s)
  
 if ans:
     print("Yes")
 else:
     print("No")
+
+    
+def next_palindrome(value):
+    while not check_if_palindrome(str(value)):
+        value =int(value) +1
+    return value
