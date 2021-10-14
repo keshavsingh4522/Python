cd8e07cc7d988bbd5a3e7dc38bbbba0bea02f27c
Mahesh B <43287976+MaheshBabu11@users.noreply.github.com>
2021-10-14 19:16:28 +0530

    Created pattern hollow_rectangle

---
+++ pattern/hollow_rectangle.py
@@ -0,0 +1,39 @@
+n=10
+for row in range(n):
+    if (row==0 or row==n-1):
+        for i in range(n):
+            print("*",end=" ")
+        print("\n")
+    else:
+        for i in range(n):
+            if(i==0 or i==n-1):
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+        print("\n")
+
+'''
+
+output :
+
+* * * * * * * * * * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+*                 * 
+
+* * * * * * * * * * 
+
+'''
