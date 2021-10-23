2be1b400282feae2924163b8a8f08e80defaa4b1
Santa Bhai <93035457+santabhai@users.noreply.github.com>
2021-10-23 16:15:53 +0530

    Adding python program that finds square root

---
+++ square-root.py
@@ -0,0 +1,7 @@
+# This program calculates the square root
+
+# Take input from the user
+number = float(input('Enter a number: '))
+
+number_sqrt = number ** 0.5
+print('The square root of %0.3f is %0.3f'%(number ,number_sqrt))
